01b0b5d2e21e9efa1caef790df68b816ebae48a0
Aditya Singhal <143562818+Ad12y@users.noreply.github.com>
2024-05-26 14:46:16 +0530

    Time: 988 ms (76.44%), Space: 0B (100.00%) - LeetHub

--- 0262-trips-and-users/0262-trips-and-users.sql
+++ 0262-trips-and-users/0262-trips-and-users.sql
@@ -1,11 +1,32 @@
-# Write your MySQL query statement below
-Select request_at as Day, ROUND(avg(Final),2) as 'Cancellation Rate' FROM (
-select request_at,
-Case
-    WHEN status != 'completed' AND banned_clients = 'No' AND banned_drivers = 'No'
-    THEN 1
-    WHEN banned_clients = 'No' AND banned_drivers = 'No'
-    THEN 0
-    ELSE NULL
-END as Final
-FROM (Select status, request_at, banned_clients, U.banned as banned_drivers from (Select driver_id, status, request_at, banned as banned_clients from Trips T left join Users U on T.client_id = U.users_id) as D left join Users U on U.users_id = D.driver_id where request_at <= '2013-10-03' and request_at >= '2013-10-01') as temp) as temp2 where Final is not NULL group by Day
+SELECT 
+    request_at AS Day, 
+    ROUND(AVG(Final), 2) AS 'Cancellation Rate'
+FROM (
+    SELECT 
+        request_at,
+        CASE
+            WHEN status != 'completed' AND banned_clients = 'No' AND banned_drivers = 'No' THEN 1
+            WHEN banned_clients = 'No' AND banned_drivers = 'No' THEN 0
+            ELSE NULL
+        END AS Final
+    FROM (
+        SELECT 
+            status, 
+            request_at, 
+            banned_clients, 
+            U.banned AS banned_drivers 
+        FROM (
+            SELECT 
+                driver_id, 
+                status, 
+                request_at, 
+                banned AS banned_clients 
+            FROM Trips T 
+            LEFT JOIN Users U ON T.client_id = U.users_id
+        ) AS D 
+        LEFT JOIN Users U ON U.users_id = D.driver_id 
+        WHERE request_at <= '2013-10-03' AND request_at >= '2013-10-01'
+    ) AS temp
+) AS temp2 
+WHERE Final IS NOT NULL 
+GROUP BY Day;
