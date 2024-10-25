36ad2efade12d4121ef2e55326b24a4d83192fb5
Aditya Singhal <143562818+Ad12y@users.noreply.github.com>
2024-10-25 10:27:15 -0400

    Time: 0 ms (100%), Space: 16.5 MB (90.06%) - LeetHub

--- 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.py
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.py
@@ -1,34 +1,20 @@
-class Solution(object):
-    def letterCombinations(self, digits):
-        dic = keyboard = {
-        "2": "abc",
-        "3": "def",
-        "4": "ghi",
-        "5": "jkl",
-        "6": "mno",
-        "7": "pqrs",
-        "8": "tuv",
-        "9": "wxyz"}
-        stack = []
-        res = []
+class Solution:
+    def letterCombinations(self, digits: str) -> List[str]:
+        dic = {"2":"abc", "3":"def", "4":"ghi", "5":"jkl", "6":"mno", "7":"pqrs", "8":"tuv", "9":"wxyz"}
+        self.res = []
+        if digits == "":
+            return []
 
-        def recur(i):
-            if len(digits) == 0:
-                return res
-            if i>=len(digits):
-                res.append(''.join(stack[:]))
-                return 
-            
+        def recur(lst, i):
+            if i > len(digits)-1:
+                self.res.append("".join(lst))
+                return
             for j in dic[digits[i]]:
-                stack.append(j)
-                recur(i+1)
-                stack.pop()
+                lst.append(j)
+                recur(lst, i+1)
+                lst.pop()
+        recur([],0)
+        return self.res
 
-        recur(0)
-        return res 
 
-        """
-        :type digits: str
-        :rtype: List[str]
-        """
         
