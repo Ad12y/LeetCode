15b31d03b195cad193568a0c7fc19d779979d677
Aditya Singhal <143562818+Ad12y@users.noreply.github.com>
2024-10-27 01:15:14 -0400

    Time: 31 ms (36.12%), Space: 17.8 MB (36.13%) - LeetHub

--- 0695-max-area-of-island/0695-max-area-of-island.py
+++ 0695-max-area-of-island/0695-max-area-of-island.py
@@ -2,31 +2,23 @@ class Solution:
     def maxAreaOfIsland(self, grid: List[List[int]]) -> int:
         m = len(grid)
         n = len(grid[0])
-        
-        def recur(i, j, visited):
-            # Check bounds and validity
+        def recur(i, j, sum_, set_):
             if i < 0 or j < 0 or i >= m or j >= n:
                 return 0
-            if grid[i][j] == 0 or (i,j) in visited:
+            if grid[i][j] == 0 or (i,j) in set_:
                 return 0
-                
-            # Mark as visited
-            visited.add((i,j))
-            
-            # Calculate area = 1 (current cell) + all adjacent cells
-            area = 1 + (recur(i+1, j, visited) +  # down
-                       recur(i-1, j, visited) +    # up
-                       recur(i, j+1, visited) +    # right
-                       recur(i, j-1, visited))     # left
-            
-            return area
-        
-        # Check each cell as a potential starting point
-        max_area = 0
-        visited = set()
-        for i in range(m):
-            for j in range(n):
-                if grid[i][j] == 1 and (i,j) not in visited:
-                    max_area = max(max_area, recur(i, j, visited))
-        
-        return max_area
+            set_.add((i,j))
+            a = recur(i+1, j, sum_+1, set_)
+            b = recur(i, j+1, sum_+1, set_)
+            c = recur(i-1, j, sum_+1, set_)
+            d = recur(i, j-1, sum_+1, set_)
+            return 1 + a + b + c + d
+        # print(recur(3, 8, 0, set()))
+
+        ans = 0
+        set_ = set()
+        for i in range(0,m):
+            for j in range(0,n):
+                if grid[i][j] and grid[i][j] not in set_:
+                    ans = max(ans, recur(i,j, 0, set_))
+        return ans
