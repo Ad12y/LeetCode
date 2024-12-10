c182113bb733a8559396d3fde48da36d18e6c137
Aditya Singhal <143562818+Ad12y@users.noreply.github.com>
2024-12-09 22:01:25 -0500

    Time: 0 ms (100%), Space: 17.3 MB (7.62%) - LeetHub

--- 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.py
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.py
@@ -1,20 +1,26 @@
 class Solution:
     def letterCombinations(self, digits: str) -> List[str]:
-        dic = {"2":"abc", "3":"def", "4":"ghi", "5":"jkl", "6":"mno", "7":"pqrs", "8":"tuv", "9":"wxyz"}
-        self.res = []
-        if digits == "":
-            return []
-
-        def recur(lst, i):
-            if i > len(digits)-1:
-                self.res.append("".join(lst))
+        dic = {
+        "2": "abc",
+        "3": "def",
+        "4": "ghi",
+        "5": "jkl",
+        "6": "mno",
+        "7": "pqrs",
+        "8": "tuv",
+        "9": "wxyz"}
+        res = []
+        def backtrack(string, i):
+            if len(string) == len(digits):
+                if len(string):
+                    res.append(string)
+            if i >= len(digits):
                 return
             for j in dic[digits[i]]:
-                lst.append(j)
-                recur(lst, i+1)
-                lst.pop()
-        recur([],0)
-        return self.res
+                backtrack(string+j, i+1)
 
+        backtrack("", 0)
+        return res
 
+            
         
