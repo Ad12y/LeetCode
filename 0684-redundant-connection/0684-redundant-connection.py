a7e102e24e9f5300796d69e652bd8a48f3addfd2
Aditya Singhal <143562818+Ad12y@users.noreply.github.com>
2024-12-09 22:09:22 -0500

    Time: 0 ms (100%), Space: 17.7 MB (22.24%) - LeetHub

--- 0684-redundant-connection/0684-redundant-connection.py
+++ 0684-redundant-connection/0684-redundant-connection.py
@@ -1,35 +1,31 @@
 class Solution:
     def findRedundantConnection(self, edges: List[List[int]]) -> List[int]:
-        n = len(edges)
-        adj_list = [[] for _ in range(n + 1)]
-        for u, v in edges:
-            adj_list[u].append(v)
-            adj_list[v].append(u)
         
-        print(adj_list)
-        start = -1
-        cycle = set()
-        visited = [False] * (n + 1)
-        def dfs(node,par):
-            nonlocal start 
-            if visited[node]:
-                start = node
-                return True
-            visited[node] = True
-            for neigh in adj_list[node]:
-                if neigh == par:
-                    continue
-                if dfs(neigh, node):
-                    if start != -1:
-                        cycle.add(node)
-                    if node == start:
-                        return
-                    return True
-        dfs(1, -1)
-        for u,v in reversed(edges):
-            if u in cycle and v in cycle:
-                return [u,v]
-        return []
+        #Trying union find to solve this problem
+        rank = [1] *(len(edges)+1)
+        par = [i for i in range(0, len(edges)+1)]
+        print(rank, par)
 
+        def find(node):
+            p = par[node] 
+            while p!=par[p]:
+                par[p] = par[par[p]]
+                p = par[p]
+            return p 
 
-        
+        def union(a,b):
+            a, b = find(a), find(b)
+            if a == b:
+                return False
+            if rank[a] > rank[b]:
+                par[b] = par[a]
+                rank[a] += rank[b]
+            else:
+                par[a] = par[b]
+                rank[b] += rank[a]
+            return True
+
+        for u,v in edges:
+            if not union(u,v):
+                return [u,v] 
+
