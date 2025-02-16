63ec84e19bde15142bfffb8a7e768ea3829a45d6
Aditya Singhal <143562818+Ad12y@users.noreply.github.com>
2025-02-16 00:13:24 -0500

    Time: 158 ms (56.01%), Space: 18.8 MB (93.48%) - LeetHub

--- 0875-koko-eating-bananas/0875-koko-eating-bananas.py
+++ 0875-koko-eating-bananas/0875-koko-eating-bananas.py
@@ -1,12 +1,22 @@
 class Solution:
-    def minEatingSpeed(self, piles: List[int], h: int) -> int:        
+    def minEatingSpeed(self, piles: List[int], h: int) -> int:
         if len(piles) == h:
             return max(piles)
-        elif h/len(piles) >= 2:
-            print("dfd")
-            return math.ceil((sum(piles)/h))    
-        elif len(piles) < h:
-            piles.sort()
-            last = h - len(piles) +1
-            return piles[-last]
+        min_val = math.ceil((sum(piles)/h))
+        max_val = max(piles)
+        res = float('inf')
+        while min_val <= max_val:
+            mid = int((min_val+max_val)/2)
+            hours_taken = 0
+            for pile in piles:
+                hours_taken += math.ceil(pile/mid)
+            if hours_taken > h:
+                min_val = mid+1
+            else:
+                res = min(res, mid)
+                max_val = mid-1
+        return res
+
+
+        
         
