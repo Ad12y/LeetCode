4d374254cc5c9370078c7a30a0df9a4d324a3164
Aditya Singhal <143562818+Ad12y@users.noreply.github.com>
2025-06-17 19:44:29 -0400

    Time: 10 ms (79.38%), Space: 31.7 MB (29.46%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.py
+++ 0217-contains-duplicate/0217-contains-duplicate.py
@@ -1,12 +1,4 @@
-class Solution(object):
-    def containsDuplicate(self, nums):
-        """
-        :type nums: List[int]
-        :rtype: bool
-        """
-        if len(set(nums)) == len(nums):
-            return False
-
-        return True
-        
+class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        return len(set(nums)) != len(nums)
         
