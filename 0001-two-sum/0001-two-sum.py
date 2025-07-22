34b3805127923535509bceb66705fac5fd5bfa75
Aditya Singhal <143562818+Ad12y@users.noreply.github.com>
2025-07-22 12:05:28 -0400

    Time: 3 ms (50.42%), Space: 19.1 MB (10.29%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -1,9 +1,9 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
         dic = {}
-        for i in range(0,len(nums)):
+        for i in range(0, len(nums)):
             dic[nums[i]] = i
-        for i in range(0,len(nums)):
-            if (target - nums[i]) in dic.keys() and dic[target - nums[i]] != i:
-                return [i, dic[target - nums[i]]]     
         
+        for i in range(0, len(nums)):
+            if target - nums[i] in dic.keys() and dic[target - nums[i]] != i:
+                return [i,dic[target - nums[i]]] 
